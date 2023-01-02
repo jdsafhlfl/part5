30bad205494b475e333c41a8e9fadd5b48ba58d8
jdsafhlfl <weiyi.xu@helsinki.fi>
2023-01-02 15:01:41 +0200

    exercise5.3

--- bloglist-frontend/src/App.js
+++ bloglist-frontend/src/App.js
@@ -9,18 +9,23 @@ const App = () => {
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
 
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
+
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs(blogs)
     )
   }, [])
 
-  useEffect(() => {    
-    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')    
-    if (loggedUserJSON) {      
-      const user = JSON.parse(loggedUserJSON)      
-      setUser(user)      
-    }  
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('loggedBlogappUser')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
   }, [])
 
   const handleLogin = async (event) => {
@@ -31,6 +36,7 @@ const App = () => {
       })
       window.localStorage.setItem('loggedBlogappUser', JSON.stringify(user))
       setUser(user)
+      blogService.setToken(user.token)
       setUsername('')
       setPassword('')
     } catch (exception) {
@@ -38,9 +44,29 @@ const App = () => {
     }
   }
 
-  const handleLogout = ()=>{
+  const handleLogout = () => {
     window.localStorage.removeItem('loggedBlogappUser')
     setUser(null)
+  }
+
+  const handleCreate = (event) => {
+    event.preventDefault()
+    const newBlog = { title: title, author: author, url: url }
+    try {
+      blogService.create(newBlog)
+        .then(response => {
+          console.log('create successfully')
+          blogService.getAll().then(blogs =>
+            setBlogs(blogs)
+          )
+        }
+      )
+      setAuthor('')
+      setTitle('')
+      setUrl('')
+    } catch (exception) {
+      console.log(exception)
+    }
   }
 
   if (user === null) {
@@ -63,9 +89,22 @@ const App = () => {
   return (
     <div>
       <h2>blogs</h2>
-      <p>{user.name} logged in 
+      <p>{user.name} logged in
         <button onClick={handleLogout}>logout</button>
       </p>
+      <h2>create new</h2>
+      <form onSubmit={handleCreate}>
+        <div>title:
+          <input type="text" value={title} name="Title" onChange={({ target }) => setTitle(target.value)} />
+        </div>
+        <div>author:
+          <input type="text" value={author} name="Author" onChange={({ target }) => setAuthor(target.value)} />
+        </div>
+        <div>url:
+          <input type="text" value={url} name="Url" onChange={({ target }) => setUrl(target.value)} />
+        </div>
+        <button type="submit">create</button>
+      </form>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
