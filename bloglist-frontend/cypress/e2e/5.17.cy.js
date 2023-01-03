6a8f93b3db8f080777b6bd060b3177ccd2f4f20b
jdsafhlfl <weiyi.xu@helsinki.fi>
2023-01-03 21:42:37 +0200

    exercise5.19

--- bloglist-frontend/cypress/e2e/5.17.cy.js
+++ bloglist-frontend/cypress/e2e/5.17.cy.js
@@ -36,4 +36,22 @@ describe('Blog app', function() {
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
   })
+
+  describe('When logged in', function() {
+    beforeEach(function() {
+      // log in user here
+      cy.get('#username').type('root')
+      cy.get('#password').type('secret')
+      cy.contains('login').click()
+    })
+
+    it('A blog can be created', function() {
+      cy.contains('create new blog').click()
+      cy.get('#title').type('Hello world')
+      cy.get('#author').type('linux')
+      cy.get('#url').type('www.helsinki.com')
+      cy.get('#create').click()
+      cy.contains('Hello world linux')
+    })
+  })
 })
